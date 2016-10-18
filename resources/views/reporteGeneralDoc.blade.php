fe7721541ff4e05ffdb9378b90dff0be09fbbc3b
Jair <jgt08@hotmail.com>
2016-10-17 23:45:24 -0500

    modificacion repGeneral

--- resources/views/reporteGeneralDoc.blade.php
+++ resources/views/reporteGeneralDoc.blade.php
@@ -1,55 +1,61 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Reporte General</title>
-    <link rel="stylesheet" href="{{'estiloPdf/css/pdfDocente.css'}}" media="all" />
-  </head>
-  <body>
-    <header class="clearfix">
-      <div id="logo">
-        <img src="img/logo_10A_FullColor.png">
-      </div>
-      <h1>Reporte General</h1>
-      <div id="company" class="clearfix">
-        <div>Universidad Ceprog</div>
-        <div>Carretera Palenque - Catazajá Km <br /> 26+500 a un costado del Aeropuerto </div>
-        <div>+52 (916) 345 3906 </div>
-        <div><a href="#">contacto@uceprog.edu.mx</a></div>
-      </div>
-      <div id="project">
-        <div><span>Proyecto</span> Evaluacion docente</div>
-        <div><span>Fecha</span>{{$fecha}}</div>
-        <div><span>Lineamientos:</span>4 puntaje maximo</div>
-        <div><span>Planeacion:</span>4 puntaje maximo</div>
-        <div><span>Motivacion:</span>4 puntaje maximo</div>
-        <div><span>Desenvolvimiento de la asignatura:</span>7 puntaje maximo</div>
-      </div>
-    </header>
-    <main>
-      <table>
-        <thead>
-          <tr>
-            <th class="service">Materias</th>
-            <th>Maestro</th>
-             @foreach($rangos as $rango)
-              <th class="desc">{{$rango->name}}</th>
-              @endforeach
-              <th>Total</th>
-              <th>Alumnos</th>
-              <th>Porcentaje</th>
-          </tr>
-        </thead>
-        <tbody>
-           @foreach($materias as $materia)
-           <?php $porcentaje =0;?>
-               <tr>
-                 <td class="service">{{$materia->name}}</td>
-                 <div class="page-break"></div>
-                 @foreach($materia->users as $user)
-                    <td class="service">{{$user->name}}</td>
-                 @endforeach
-                 @foreach($rangos as $rango)
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Reporte General</title>
+            <link rel="stylesheet" href="{{'estiloPdf/css/pdfDocente.css'}}" media="all" />
+        </head>
+        <body>
+        <header class="clearfix">
+            <div id="logo">
+                <img src="img/logo_10A_FullColor.png">
+            </div>
+            <h1>Reporte General</h1>
+            <div id="company" class="clearfix">
+                <div>Universidad Ceprog</div>
+                <div>Carretera Palenque - Catazajá Km <br /> 26+500 a un costado del Aeropuerto </div>
+                <div>+52 (916) 345 3906 </div>
+                <div><a href="#">contacto@uceprog.edu.mx</a></div>
+            </div>
+            <div id="project">
+                <div><span>Proyecto</span> Evaluacion docente</div>
+                <div><span>Fecha</span>{{$fecha}}</div>
+                <div><span>Lineamientos:</span>4 puntaje maximo</div>
+                <div><span>Planeacion:</span>4 puntaje maximo</div>
+                <div><span>Motivacion:</span>4 puntaje maximo</div>
+                <div><span>Desenvolvimiento de la asignatura:</span>7 puntaje maximo</div>
+                @foreach($materias as $materia)
+                  <div><span>Suma de los totales de cada materia: </span>{{number_format($suma/$materia->cont,1)}}%</div>
+                @endforeach
+            </div>
+        </header>
+        <main>
+        {{-- */ $count=1 /* --}}
+        {{-- */ $lineaspag=10 /* --}}
+        @foreach($materias as $materia)
+            @if($count==1)
+                <table class="page-break">
+                    <thead>
+                    <tr>
+                        <th class="service">Materias</th>
+                        <th>Maestro</th>
+                        @foreach($rangos as $rango)
+                            <th class="desc">{{$rango->name}}</th>
+                        @endforeach
+                        <th>Total</th>
+                        <th>Alumnos</th>
+                        <th>Porcentaje</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @endif
+                    <?php $porcentaje = 0;?>
+                    <tr>
+                        <td class="service">{{$materia->name}}</td>
+                        @foreach($materia->users as $user)
+                          <td class="service">{{$user->name}}</td>
+                        @endforeach
+                        @foreach($rangos as $rango)
                   <?php $total=0;?>
                     @foreach($rango->preguntas as $pregunta)
                       @foreach($pregunta->respuestasDocentes as $posResp)
@@ -64,23 +70,20 @@
                     <td class="service">{{number_format($total/$materia->mat_results,1)}}%</td>
                     <?php $porcentaje += $total;?>
                  @endforeach
-                  <td class="service">{{number_format($porcentaje/$materia->mat_results,1)}}</td>
+                 <td class="service">{{number_format($porcentaje/$materia->mat_results,1)}}</td>
                   <td class="service">{{$materia->mat_results}}</td>
                   <td class="service">{{number_format($porcentaje/$materia->mat_results,1)}} / 25</td>
-               </tr>
-            @endforeach 
-        </tbody>
-      </table>
-      <hr>
-      <h3><strong>Suma de los totales de cada materia: </strong>{{number_format($suma/$materia->cont,1)}}%</h3>
-      <hr>
-      <div id="notices">
-        <div>NOTICE:</div>
-        <div class="notice">Este formato es un reporte General de la evaluacion al docente.</div>
-      </div>
-    </main>
-    <footer>
-      Universidad ceprog Construimos tu futuro.
-    </footer>
-  </body>
+                    </tr>
+                    {{-- */ $count=$count+1 /* --}}
+                    @if($count==$lineaspag)
+                    </tbody>
+                </table>
+ {{-- */ $count=1 /* --}}
+@endif
+@endforeach
+</main>
+<footer>
+Universidad ceprog Construimos tu futuro.
+</footer>
+</body>
 </html>
